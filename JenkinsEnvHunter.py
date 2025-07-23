c083c8ae3e588b0b893322b264074895133b3b80
Kent Ickler <29710634+Relkci@users.noreply.github.com>
2025-07-22 20:08:46 -0600

    Update JenkinsEnvHunter.py
    cleaner output with --quiet

--- JenkinsEnvHunter.py
+++ JenkinsEnvHunter.py
@@ -65,19 +65,22 @@ def main():
     total_env_vars = 0
     builds_with_sensitive = 0
     k=""
+    job_count=0
+    build_count=0
     jobs = get_all_jobs(args.url, auth_provided)
     for job in jobs:
         job_name = job["name"]
         job_url = job["url"]
         builds = get_builds_for_job(job_url, auth_provided)
-
+        job_count=job_count+1
         if not args.quiet: print(f"\n[+] Scanning job: {job_name} ({len(builds)} builds)")
         with alive_bar(len(builds), title=f"Scanning {job_name[:40].ljust(40)}",length=10,theme='smooth',spinner=None,dual_line=True,monitor="Build {count} / {total} ") as bar:
             for build in builds:
                 build_url = build["url"]
                 build_number = build.get("number", "?")
-                if args.quiet: bar.text(f"\t Total Sensitive EnvVars Discovered: {total_sensitive_vars}  ")
-                if not args.quiet: bar.text(f"\t -> Job: {job_name}  \t Build: {build_number}. **TTL Sensitive EV: {total_sensitive_vars} ")
+                build_count=build_count+1
+                if args.quiet: bar.text(f"\t Total Sensitive EnvVars Discovered: {total_sensitive_vars} -- Unique: {len(seen_values)} --  Jobs: {job_count}/{len(jobs)} -- Builds: {build_count} ")
+                if not args.quiet: bar.text(f"\t -> Job: {job_name}  \t Build: {build_number}. **TTL Sensitive EV: {total_sensitive_vars} -- Total Unique:{len(seen_values)}")
                 env_vars = get_env_vars(build_url, auth_provided)
                 findings = scan_env_vars(env_vars)
                 vars_to_report = env_vars if args.all else findings
@@ -94,18 +97,21 @@ def main():
                     value_id = f"{k}={v}"
                     if value_id not in seen_values:
                         seen_values.add(value_id)
-                        if not args.quiet: print(f"\t \033[1m [+] New value discovered: {k} = \033[0m {v}")
+                        if not args.quiet: print(f"\t \033[1m [+] New value discovered: {k} = \033[0m {v}\n")
                 if vars_to_report and output_file:
                     write_finding(output_file, build_url, vars_to_report)
 
                 bar()
 
     print("\n[✓] Scan complete.")
+    print(f"    Total Jobs {len(jobs)}      Total Builds {build_count}")
     print(f"    Builds with sensitive data: {builds_with_sensitive}")
     print(f"    Total sensitive vars found: {total_sensitive_vars}")
     if args.all:
         print(f"    Total environment vars seen: {total_env_vars}")
     print(f"    Unique values discovered: {len(seen_values)}")
+    if output_file:
+        print(f"    Variables Saved To: {output_file}")
 
 if __name__ == "__main__":
     main()
